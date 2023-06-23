fbab85ac9dc1af8983f2e4080247647a4afb492e
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-06-23 16:40:20 -0600

    the class NcchTanQ created and the needed info to run the lass added

--- NchTan/frame.py
+++ NchTan/frame.py
@@ -2,7 +2,40 @@
 the n choice and t answer quetions and we create
 some logics to work with them"""
 
+from __future__ import annotations
+
+
 
 class NchTanQ:
-    pass
+    """NchTanQ"""
+
+    question_title: str
+    choices: list | set | tuple
+    answers: list | set | tuple
+    hints: str
+    descriptions: str
+
+    def __init__(self,question_title: str,
+                      choices: list | set | tuple,
+                      answers: list | set | tuple,
+                      hints: str="",
+                      descriptions: str=""):
+        pass
+
+    @staticmethod
+    def is_each_item_in_arr():
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
 
