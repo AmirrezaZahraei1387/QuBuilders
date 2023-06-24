fe4eff5e58ed99fbec0517b79de9a02b63ec1ab7
amirrezazahraei <amirreza.zahraei1@rbe.sk.ca>
2023-06-24 15:17:21 -0600

    the csv_loader is created

--- quloaders/csv_loader.py
+++ quloaders/csv_loader.py
@@ -9,8 +9,25 @@ question_title, answers, choices, hints, descriptions
 ... and here each element"""
 
 import csv
-import os
+import QuBu.NchTan as NT
 
 
-def open_file():
-    pass
+def read_file(file_object, name:str, descr:str):
+    """the file_object is your file that you opened using
+    open. name is the name for your question seq and descr is the
+    descriptions for your question seq"""
+
+    data = csv.reader(file_object)
+    next(data)
+    obj = NT.QuSeq(name, descr)
+
+    for question in data:
+        qu = NT.QuestionDataSaver(question[0], exec(question[1]), exec(question[2]), question[3], question[4])
+        obj += qu
+
+    return obj
+
+
+
+
+
