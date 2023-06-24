ddefa727745caaf06bcba305190965483a273ba6
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-06-23 22:36:03 -0600

    the test for test_NchTan_From.py is created

--- tests/test_NchTan_From.py
+++ tests/test_NchTan_From.py
@@ -5,6 +5,11 @@ from NchTan import QuestionDataSaver
 
 class test(unittest.TestCase):
 
+    a = QuestionDataSaver("what is the synonym of stupid?", ["idiot", "donkey"],
+                          ["idiot", "donkey", "good", "awsome"],
+                          "stupid means some one with unruly behaviors",
+                          "this is for testing the vocab of students")
+
     def test_instanting(self):
 
         try:
@@ -18,4 +23,27 @@ class test(unittest.TestCase):
             b= True
 
         self.assertEqual(b, True)
+
+    def test_is_each_item_in_array(self):
+
+        b = self.a.is_each_item_in_arr(self.a.answers, self.a.choices)
+        self.assertEqual(b, True)
+
+    def test_is_present(self):
+
+        try:
+            self.a.is_present(self.a.choices, self.a.answers, "choices", "answers")
+        except ValueError:
+            b = True
+        else:
+            b = True
+
+        self.assertEqual(b, True)
+
+
+if __name__ == '__main__':
+    test.main()
+
+
+
 
