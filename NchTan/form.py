4e5fd42e5f2ba025af9d7f462b8ee3021f2c3280
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-06-23 21:55:12 -0600

    the form is created and the questiondatasaver class is changed

---
+++ NchTan/form.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+
+class QuestionDataSaver:
+
+    def __init__(self, question_title: str,
+                 choices: list | set | tuple,
+                 answers: list | set | tuple,
+                 hints: str = "",
+                 descriptions: str = ""):
+
+        self.is_present(answers, choices, "answers", "choices")
+        self.question_title = question_title
+        self.choices = choices
+        self.answers = answers
+        self.hints = hints
+        self.descriptions = descriptions
+
+    @staticmethod
+    def is_each_item_in_arr(array_1: list | set | tuple,
+                            array_2: list | set | tuple):
+        """this array  will check if each item in array_1 is in
+        array_2 or no"""
+
+        for array_1_item in array_1:
+            if array_1_item not in array_2:
+                return False
+        return True
+
+    def is_present(self, array_1: list | set | tuple,
+                   array_2: list | set | tuple, name_1: str, name_2: str):
+
+        message = "the {} must be present in {}".format(name_1, name_2)
+
+        if not self.is_each_item_in_arr(array_1, array_2):
+            raise ValueError(message)
--- NchTan/frame.py
+++ NchTan/frame.py
@@ -3,67 +3,24 @@ the n choice and t answer quetions and we create
 some logics to work with them"""
 
 from __future__ import annotations
-from dataclasses import dataclass
-
-
-@dataclass
-class QuestionDataSaver:
-    question_title: str
-    choices: list | set | tuple
-    answers: list | set | tuple
-    hints: str
-    descriptions: str
 
 
 class NchTanQ(QuestionDataSaver):
     """NchTanQ"""
 
-    question_title: str
-    choices: list | set | tuple
-    answers: list | set | tuple
-    hints: str
-    descriptions: str
-    number_choices:int
-
-    def __init__(self, question_title: str,
-                 choices: list | set | tuple,
-                 answers: list | set | tuple,
-                 hints: str = "",
-                 descriptions: str = ""):
-
-        if not self.is_each_item_in_arr(answers, choices):
-            raise ValueError("the answers must be present in  choices")
-
-        super().__init__(question_title, choices,
-                         answers, hints, descriptions, len(choices))
-
-    @staticmethod
-    def is_each_item_in_arr(array_1: list | set | tuple,
-                            array_2: list | set | tuple):
-        """this array  will check if each item in array_1 is in
-         array_2 or no"""
-
-        for array_1_item in array_1:
-            if array_1_item not in array_2:
-                return False
-
-        return True
-
     def __str__(self):
-
         complete_question = ""
         complete_question += self.question_title
 
         for choice in self.choices:
-            complete_question += '\n'+str(choice)
+            complete_question += '\n' + str(choice)
 
-        complete_question += '\n'+str(self.answers)
+        complete_question += '\n' + str(self.answers)
 
-        complete_question += '\n'+self.hints
-        complete_question += '\n'+self.descriptions
+        complete_question += '\n' + self.hints
+        complete_question += '\n' + self.descriptions
 
         return complete_question
 
-
-
-
+    def __eq__(self, other: list | set | tuple):
+        pass
