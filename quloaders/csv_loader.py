f5a6dcaf118ff6fc54ec11f7393395e2061974f6
amirrezazahraei <amirreza.zahraei1@rbe.sk.ca>
2023-06-24 15:06:05 -0600

    some decriptions are provided for csv_loader

---
+++ quloaders/csv_loader.py
@@ -0,0 +1,16 @@
+"""this code file is able to read the
+questions from a csv  file and then return
+the questions as a quseq object.
+the csv file must have the following format otherwise it is not readable by this
+reader.
+
+the header of csv should be like this:
+question_title, answers, choices, hints, descriptions
+... and here each element"""
+
+import csv
+import os
+
+
+def open_file():
+    pass
