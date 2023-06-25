206bbc6be021500910da2ef642fd8f3ad24b6f2b
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-06-25 16:17:05 -0600

    a docstring is added to json_writer.py file

---
+++ quwriter/json_writer.py
@@ -0,0 +1,13 @@
+"""this is a code file that is created to write
+the questions in a json file or in a sequence of json files
+inside a folder.
+the folder however must have the following format otherwise
+there will be an error:
+dir_name
+|
+|___question_1.json
+|___question_2.json
+|___question_3.json
+|___question_4.json
+|..."""
+
