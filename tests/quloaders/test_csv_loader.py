73a8e865578b0e980409eef10cbb0ff567e2523e
amirrezazahraei <amirreza.zahraei1@rbe.sk.ca>
2023-06-25 12:22:32 -0600

    the test for csv_loader created

--- tests/quloaders/test_csv_loader.py
+++ tests/quloaders/test_csv_loader.py
@@ -1,10 +1,14 @@
-
 from quloaders import csv_loader
 import unittest
 
 
 class test(unittest.TestCase):
-    pass
+
+    def test_read_file(self):
+        file = open("data_sample/exam.csv", 'r', encoding='utf-8')
+        qus = csv_loader.read_file(file, "hello", "the hello")
+        self.assertEqual(qus[0].choices, [12, 11, 45, 7])  # it checks if the choices of question  1 is the same as
+        # expected or no
 
 
 if __name__ == "__main__":
