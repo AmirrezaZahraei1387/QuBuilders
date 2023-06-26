cab90585e0980aaa3cfd73446da29af9bc1ee978
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-06-26 14:06:18 -0600

    the option for same questions or no added

--- generators/stament/Asghar_S.py
+++ generators/stament/Asghar_S.py
@@ -7,7 +7,7 @@ What is {} in Farsi?
 Afterward the code will need a data like the following:
 {("hello", ):["salam"], ("how are you", ):["khobi"], ("goodbye", ): ["khoda hafez"]}
 then you have to say from which part(keys or values) we should pick
-important note: pass the keys as tuples and add a , at the end otherwise
+important note: pass the keys as tuples and add a ',' at the end otherwise
 unexpected result will come up"""
 import random
 
@@ -20,7 +20,7 @@ class State:
     statement_coms: list
     statement_keys: list
 
-    def __init__(self, data_question: dict, mode: str):
+    def __init__(self, data_question: dict, mode: str, is_allowed_same: bool):
         def save(value_1, value_2):
             """this is a method that is created to give a same value in
             different ways to two vars"""
@@ -34,12 +34,14 @@ class State:
             save(data_question.values(), data_question.keys())
 
         else:
-            raise ValueError("the mode can only be value or key given "+str(mode))
+            raise ValueError("the mode can only be value or key given " + str(mode))
 
         self.length = len(self.statement_keys)
 
         if not (self.length == len(self.statement_coms)):
             raise ValueError("the length of keys and values of the data didn't match")
+
+        self.__is_allowed_same = is_allowed_same
 
     @property
     def frame(self):
@@ -47,13 +49,14 @@ class State:
         index = random.randint(0, self.length - 1)
         statement_com = self.statement_coms[index]
         statement_key = self.statement_keys[index]
+
+        if not self.__is_allowed_same:
+            self.statement_keys.remove(statement_key)
+            self.statement_coms.remove(statement_com)
+
         frame_state = self.__Frame.format(*statement_com)
         return frame_state, statement_key
 
     @frame.setter
     def frame(self, frame: str):
         self.__Frame = frame
-
-
-
-
