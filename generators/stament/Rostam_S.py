d1de8d8e706816acce328deab98976c01df88f0d
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-06-26 14:21:55 -0600

    the __init__ of class State is crated

---
+++ generators/stament/Rostam_S.py
@@ -0,0 +1,34 @@
+"""this is a code file that implements another way to
+make the question statements. So sometimes teachers give
+some questions that they look the same, and just their numbers
+or some words is changed in these cases we can use this statement
+creator to create these kinds of things"""
+import random
+from inspect import isfunction
+
+
+class State:
+    """the frame is the frame for making the questions
+    and values_formatting is th list of lists of values for
+    putting in the frame to make some sort of statement
+    function_solving is a function that get the values and return the
+    answer. the number of parameters of the function must be equal to
+    the number of values are going to be formatted in  the string"""
+
+    __frame: str
+
+    def __init__(self, frame: str, values_formatting: list, function_solving):
+
+        if not isfunction(function_solving):
+            raise TypeError("the value given as a function is not a function")
+
+        self.frame = frame
+        self.values_formatting = values_formatting
+        self.function_solving = function_solving
+
+
+
+
+
+
+
