e58caed1bf2db587f5e59ba858d55b019dd332b3
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-06-27 09:41:26 -0600

    __init__ created

---
+++ generators/stament/Mode_S.py
@@ -0,0 +1,19 @@
+"""the Mode_S is a kind of statement for making
+a question that the statement will have some different conditions.
+For, example it can be true or false so the statement mode is true or
+false"""
+
+
+class ModeState:
+    """the statements must be dictionary. the dictionary
+    must be like the following:
+    [statement_1, statement_2, statement_3, statement_4]
+    [mode_1, mode_2, mode_3, mode_4]
+    """
+
+    def __init__(self, statements: list, modes: list, is_allowed_same: bool):
+        self.statements = statements
+        self.modes = modes  # these modes are the modes of each statement
+        self.modes_in = set(self.modes)  # modes_in is the all possible modes
+        self.is_allowed_same = is_allowed_same
+
