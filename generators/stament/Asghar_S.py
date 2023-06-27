f3b14df8368d30db2cbf8a234b0ceeaea8552067
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-06-27 10:19:25 -0600

    the codes are checked

--- generators/stament/Asghar_S.py
+++ generators/stament/Asghar_S.py
@@ -9,54 +9,20 @@ Afterward the code will need a data like the following:
 then you have to say from which part(keys or values) we should pick
 important note: pass the keys as tuples and add a ',' at the end otherwise
 unexpected result will come up"""
-import random
-
-
-class check_length:
-
-    length: int
-
-    def __init__(self, array_1: list, array_2: list, is_allowed_same: bool):
-        self.array_1 = array_1
-        self.array_2 = array_2
-        self.__is_allowed_same = is_allowed_same
-
-    def check_length_save(self):
-        """this method will check the length of statement_keys and
-        statement_coms, and it will save one of them"""
-        self.length = len(self.array_1)
-
-        if not (self.length == len(self.array_2)):
-            raise ValueError("the length of keys and values of the data didn't match")
-
-    def check_end(self):
-        if len(self.array_1) == 0:
-            return "NVQ"
-
-    def pick_random(self):
-
-        index = random.randint(0, self.length - 1)
-        value_1 = self.array_1[index]
-        value_2 = self.array_2[index]
-
-        if not self.__is_allowed_same:
-            self.array_1.pop(index)
-            self.array_2.pop(index)
-            self.length -= 1
-
-        return value_1, value_2
+from checker import check_length
 
 
 class AsgharState(check_length):
     """mode defines from keys or values we should pick.
     key means key
     value means value"""
-    __Frame: str
+    __Frame: str = ""
     statement_coms: list
     statement_keys: list
-    length:int
+    length: int
 
     def __init__(self, data_question: dict, mode: str, is_allowed_same: bool):
+
         def save(value_1, value_2):
             """this is a method that is created to give a same value in
             different ways to two vars"""
@@ -72,17 +38,14 @@ class AsgharState(check_length):
         else:
             raise ValueError("the mode can only be value or key given " + str(mode))
 
-        super().__init__(self.statement_coms, self.statement_keys, self.__is_allowed_same)
+        super().__init__(self.statement_coms, self.statement_keys, is_allowed_same)
 
         self.check_length_save()
-
-        self.__is_allowed_same = is_allowed_same
-
 
     @property
     def frame(self):
 
-        if len(self.statement_keys) == 0:
+        if len(self.array_1) == 0:
             return "NVQ"
 
         statement_com, statement_key = self.pick_random()
@@ -91,5 +54,6 @@ class AsgharState(check_length):
         return frame_state, statement_key
 
     @frame.setter
-    def frame(self, frame: str):
-        self.__Frame = frame
+    def frame(self, frame_: str):
+        self.__Frame = frame_
+
---
+++ generators/stament/checker.py
@@ -0,0 +1,30 @@
+import random
+
+
+class check_length:
+
+    def __init__(self, array_1: list, array_2: list, is_allowed_same: bool):
+        self.array_1 = array_1
+        self.array_2 = array_2
+        self.__is_allowed_same = is_allowed_same
+
+    def check_length_save(self):
+        """this method will check the length of statement_keys and
+        statement_coms, and it will save one of them"""
+        self.length = len(self.array_1)
+
+        if not (self.length == len(self.array_2)):
+            raise ValueError("the length of keys and values of the data didn't match")
+
+    def pick_random(self):
+
+        index = random.randint(0, self.length - 1)
+        value_1 = self.array_1[index]
+        value_2 = self.array_2[index]
+
+        if not self.__is_allowed_same:
+            self.array_1.pop(index)
+            self.array_2.pop(index)
+            self.length -= 1
+
+        return value_1, value_2
