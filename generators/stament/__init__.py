3162b6e0ab213d817ffc7eca774edbea29a20612
amirrezazahraei <amirreza.zahraei1@rbe.sk.ca>
2023-06-28 18:11:08 -0600

    the init for the statement is created

--- generators/stament/__init__.py
+++ generators/stament/__init__.py
@@ -1,5 +1,5 @@
 
 from generators.stament.Asghar_S import AsgharState
-from generators.stament.Rostam_S import RostamState
 from generators.stament.Mode_S import ModeState
 from generators.stament.checker import Check_Length
+from generators.stament.Rostam_S import RostamState
