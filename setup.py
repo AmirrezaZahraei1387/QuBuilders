2ecefcb72377bf9bf52bafc1f53609ff86ba8d60
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-03 21:17:42 -0600

    the filename changed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'QuBu',         # How you named your package folder (MyLib)
-  packages = ['QuBuilders'],   # Chose the same as "name"
+  packages = ['QuBu'],   # Chose the same as "name"
   version = '0.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'this is a python package help you to make n choice questions',   # Give a short description about your library
